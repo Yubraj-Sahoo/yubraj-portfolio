d95fdd49f8d7261ae428b49dbe8ccf43c1bffd6b
yubraj <yubraj2000yubraj@gmail.com>
2026-08-08 23:05:01 +0530

    Update Vite config to set base path for GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: process.env.GITHUB_ACTIONS
+      ? "/yubraj-portfolio/"
+      : "/",
   test: {
     globals: true,
     environment: "jsdom",
